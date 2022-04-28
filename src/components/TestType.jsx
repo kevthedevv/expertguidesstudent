6c522fecf18df9cd5d342027b116ae7721c9ec12
Kevin <kevin.esposado@gmail.com>
2022-04-29 02:20:29 +0800

    New

---
+++ src/components/TestType.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components"
+import cardImage from "../images/card.jpg"
+
+import {Button, P as Counter, SpaceBetweenDiv, CenterDiv} from '../styles'
+import { SpeedOutlined } from '@material-ui/icons'
+
+const Card = styled.div`
+    height: 420px;
+    width: 350px;
+    background-color: white;
+    border-radius: 5%;
+    overflow: hidden;
+    box-shadow: 0px 2px 8px #e4e4e4;
+    position: relative;
+`
+const Image = styled.img`
+    height: 340px;
+    width: 350px;
+`
+const Icon = styled.img`
+   height: 150px;
+    width: 150px;
+`
+const Info = styled.div`
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+`
+const Title = styled.h1`
+   font-size: 20px;
+   font-weight: 100;
+   color: white;
+   text-transform: uppercase;
+   letter-spacing: 5px;
+`
+
+
+const TestType = ({title, source, itemscount}) => {
+  return (
+    <Card style={{margin: "10px"}}>
+         <Info>
+           <CenterDiv> 
+             <Title>{title}</Title>
+           </CenterDiv>
+           <CenterDiv> 
+          <Icon src={source}/>
+          </CenterDiv>
+        </Info>
+        <Image src={cardImage}/>
+            <SpaceBetweenDiv style={{marginTop: "20px", padding: "0 10px 0 10px"}}>
+                <Counter>{itemscount}</Counter>
+                <Button>Start</Button>
+            </SpaceBetweenDiv>
+    </Card> 
+  )
+}
+
+export default TestType
---
+++ src/styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components"
+
+export const Button = styled.button`
+    padding: 10px;
+    height: 45px;
+    background-color: #1E508B;
+    color: white;
+    cursor: pointer;
+    border: 0;
+    border-radius: 5px;
+    &:hover,&:focus{
+        background-color: #6179b8;
+        outline: none;
+    }
+`
+export const CenterDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`
+export const LeftDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: left;
+`
+export const P = styled.p`
+    color: gray;
+    padding: 0;
+    margin: 0;
+`
+export const RightDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: right;
+`
+export const SpaceAroundDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`
+export const SpaceBetweenDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const Input = styled.input`
+    border: 0;
+    background-color: transparent;
+    outline: none;
+    font-size: 1rem;
+    width: 300px;
+`
+export const InputContainer = styled.div`
+    display: flex;
+    padding: 10px;
+    width: 300px;
+    border: 1px solid #bbbbbb;
+`
